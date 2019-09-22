9cfb292319ae7288adc7276c5318f467e64adf55
ddehueck <d.dehueck@gmail.com>
2019-09-22 08:56:51 -0500

    Added experiment class

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dynaml-lib-ddehueck",
+    name="dynaml-lib",
     version="0.0.1",
     author="Cole Morgan, Devin de Hueck, Henry Creamer, Jonathon Cai",
     author_email="d.dehueck@gmail.com",
